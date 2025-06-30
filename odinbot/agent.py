5ceb6445217a718f7f98b7e97deeed80db2a3eb9
Mario Cariñana <macaab26@gmail.com>
2025-06-30 18:06:59 +0200

    Changing did

--- odinbot/agent.py
+++ odinbot/agent.py
@@ -9,7 +9,7 @@ from discord import app_commands
 from discord.ext import commands
 from datetime import datetime
 from any_agent import AgentConfig, AnyAgent
-from any_agent.config import MCPStdio
+from any_agent.config import MCPSse
 from any_agent.tools import search_web, visit_webpage
 from pydantic import BaseModel, Field
 from odinbot.tools.odin import check_submission, get_threatfeed
@@ -180,27 +180,9 @@ class MessageAnalyzerBot(commands.Bot):
                 model_id="o3",
                 instructions=instructions,
                 tools=[
-                    MCPStdio(
-                        command="docker",
-                        args=[
-                            "run",
-                            "-i",
-                            "--rm",
-                            "-e",
-                            "DISCORD_TOKEN",
-                            "mcp/mcp-discord",
-                        ],
-                        env={
-                            "DISCORD_TOKEN": os.getenv("DISCORD_TOKEN"),
-                        },
-                        tools=[
-                            "test",
-                            "discord_read_messages",
-                            "discord_login",
-                            "discord_send",
-                        ],
-                        client_session_timeout_seconds=30.0,  # Increased timeout to 60 seconds
-                    ),
+                    MCPSse(
+                        url= os.environ.get('MCP_DISCORD', 'http://mcp-discord:80'),
+                    ), 
                     check_submission,
                     get_threatfeed,
                     search_web,
