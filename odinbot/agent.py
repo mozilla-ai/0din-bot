fd3ef1d7a0995780a073d3978a7f3322307276ea
I. Vidal <ividal@users.noreply.github.com>
2025-07-17 08:37:19 +0200

    feature(agent): use streamable http with discord mcp server
    feature(agent): agent uses Discord MCP server via streamable http + docker compose

--- odinbot/agent.py
+++ odinbot/agent.py
@@ -9,7 +9,7 @@ from discord import app_commands
 from discord.ext import commands
 from datetime import datetime
 from any_agent import AgentConfig, AnyAgent
-from any_agent.config import MCPSse
+from any_agent.config import MCPStreamableHttp
 from any_agent.tools import search_web, visit_webpage
 from pydantic import BaseModel, Field
 from odinbot.tools.odin import check_submission, get_threatfeed
@@ -190,7 +190,7 @@ class MessageAnalyzerBot(commands.Bot):
                 model_id="o3",
                 instructions=instructions,
                 tools=[
-                    MCPSse(
+                    MCPStreamableHttp(
                         url= MCP_DISCORD_URL,
                     ), 
                     check_submission,
